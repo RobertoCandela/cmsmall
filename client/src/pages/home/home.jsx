2060370397d545120b50e9470d9e7ab1729e157c
RobertoCandela <roberto.candela@hotmail.it>
2023-06-17 16:17:57 +0200

    adding css

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./home.css";
 import { getPages } from "../../service/page-service";
 import PageCard from "../../components/page-card";
+import Button from "@mui/material/Button";
 
 function Home() {
   const [dataList, setDataList] = useState([]);
@@ -11,17 +12,26 @@ function Home() {
       setDataList(resp);
       console.log(resp);
     });
-    
   };
   //viene fatto solo al primo render della pagina perchè le quadre sono vuote e non ci sono variabili che possono cambiare
   useEffect(() => {
-    getPagesHandler()
+    getPagesHandler();
   }, []);
 
   return (
-    <div className="grid">
-      {dataList && dataList.map((data,index) => <PageCard data={data} getPages={getPagesHandler} key={index}></PageCard>)}
-    </div>
+    // <div style={{display:"flex", alignItems:"center", flexDirection:"column", width:"100vw"}}>
+      <div className="grid">
+        {dataList &&
+          dataList.map((data, index) => (
+            <PageCard
+              data={data}
+              getPages={getPagesHandler}
+              key={index}
+            ></PageCard>
+          ))}
+      </div>
+      //<Button variant="contained" sx={{marginTop:"30px", maxWidth:"150px"}}>Create New Page</Button>
+    // </div>
   );
 }
 export default Home;
