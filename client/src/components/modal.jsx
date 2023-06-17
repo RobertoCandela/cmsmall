ecd10bded3032edb2ec741fb7016b9e6bb4eeeb6
RobertoCandela <roberto.candela@hotmail.it>
2023-06-17 02:34:19 +0200

    managing delete with modal

---
+++ client/src/components/modal.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+export default function Modal({
+  title,
+  content,
+  setOpen,
+  open,
+  onConfirm,
+}) {
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {content}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Discard</Button>
+        <Button onClick={()=>{onConfirm(); handleClose()}} autoFocus sx={{color:"red"}}>
+          Confirm
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
--- client/src/components/page-card.jsx
+++ client/src/components/page-card.jsx
@@ -7,63 +7,73 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { EditPageSVG } from "../assets/editPageSVG";
 import { deletePage } from "../service/page-service";
+import Modal from "./modal";
 //TODO: manage edit and delete based on user profiles
-function PageCard({ data, getPages}) {
+function PageCard({ data, getPages }) {
   const [hover, setHover] = useState(false);
+  const [open, setOpen] = useState(false);
   const history = useNavigate();
-  const handleDelete = async () =>{
+  const handleDelete = async () => {
+    const status = await deletePage(data.id);
 
-    const status = await deletePage(data.id)
-
-    if(status){
-      getPages()
+    if (status) {
+      getPages();
     }
-  }
+  };
   return (
-    <Link
-      to={`/page/${data.id}`}
-      className="card theme"
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-    >
-      <div className="overlay"></div>
-      <div className="circle">
-        <PageSVG fill={"white"} width="40%" height="40%" />
-      </div>
-      <div className="container">
-        <Typography className="typography strong">{data.title}</Typography>
-        <Typography className="typography light">
-          Author: {data.username}
-        </Typography>
-        <Typography className="typography light">
-          Publication date: {data.publication_date}
-        </Typography>
-      </div>
-      {hover && (
-        <div style={{position:"absolute",top: 5, right: 5, zIndex: 9999}}>
-        <IconButton
-          aria-label="edit"
-          onClick={(ev) => {
-            ev.preventDefault();
-            ev.stopPropagation();
-            history(`/modifyPage/${data.id}`);
-          }}
-        >
-          <EditIcon sx={{color:"white"}} />
-        </IconButton>
-        <IconButton
-          aria-label="delete"
-          onClick={(ev) => {
-            ev.preventDefault();
-            ev.stopPropagation();
-            handleDelete()
-          }}
-        >
-          <DeleteIcon sx={{color:"white"}} />
-        </IconButton>
+    <>
+      <Link
+        to={`/page/${data.id}`}
+        className="card theme"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        <div className="overlay"></div>
+        <div className="circle">
+          <PageSVG fill={"white"} width="40%" height="40%" />
         </div>
-      )}
-    </Link>
+        <div className="container">
+          <Typography className="typography strong">{data.title}</Typography>
+          <Typography className="typography light">
+            Author: {data.username}
+          </Typography>
+          <Typography className="typography light">
+            Publication date: {data.publication_date}
+          </Typography>
+        </div>
+        {hover && (
+          <div style={{ position: "absolute", top: 5, right: 5, zIndex: 9999 }}>
+            <IconButton
+              aria-label="edit"
+              onClick={(ev) => {
+                ev.preventDefault();
+                ev.stopPropagation();
+                history(`/modifyPage/${data.id}`);
+              }}
+            >
+              <EditIcon sx={{ color: "white" }} />
+            </IconButton>
+            <IconButton
+              aria-label="delete"
+              onClick={(ev) => {
+                ev.preventDefault();
+                ev.stopPropagation();
+                setOpen(true)
+              }}
+            >
+              <DeleteIcon sx={{ color: "white" }} />
+            </IconButton>
+          </div>
+        )}
+      </Link>
+      <Modal
+        title="Confirm Delete"
+        content={`Do you want to delete Page ${data.title} ?`}
+        setOpen={setOpen}
+        open={open}
+        onConfirm={handleDelete}
+      ></Modal>
+    </>
   );
 }
 
