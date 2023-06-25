53e620b6bb331598d6892f6653bda2d52d66e6e1
RobertoCandela <roberto.candela@hotmail.it>
2023-06-25 04:23:16 +0200

    nomatch route

--- client/src/pages/nomatch/nomatch.jsx
+++ client/src/pages/nomatch/nomatch.jsx
@@ -1,11 +1,14 @@
+import { Typography } from '@mui/material';
+import { NoMatchSVG } from '../../../assets/nomatchSVG';
 import './nomatch.css' 
 
 function NoMatch(){
 
     return (
-        <>
-        NoMatch
-        </>
+        <div style={{display:"flex",justifyContent:"center",flexDirection:"column",alignItems:"center"}}>
+        <NoMatchSVG/>
+        <Typography variant="h2" sx={{marginTop:"20px"}}>No page found!</Typography>
+        </div>
     );
     
 
