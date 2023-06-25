67cc88f9634844414316d66b065884dac617fa3c
RobertoCandela <roberto.candela@hotmail.it>
2023-06-25 21:33:59 +0200

    fix caps

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -17,7 +17,7 @@ import {
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import "./Login.css";
+import "./login.css";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 function Login({ login }) {
